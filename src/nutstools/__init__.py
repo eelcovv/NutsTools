ea50a5b25fdef41f2b7591a536930a1601a7f907
EVLT <EVLT@cbs.nl>
2023-11-26 14:55:31 +0100

    fixed version option

--- src/nutstools/__init__.py
+++ src/nutstools/__init__.py
@@ -8,7 +8,7 @@ else:
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "EUNuts"
+    dist_name = "NutsTools"
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
