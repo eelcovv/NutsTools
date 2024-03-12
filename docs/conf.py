dfa90297a102932e7ba4e0e72c2b35f64c1f261e
EVLT <evlt@cbs.nl>
2024-03-12 15:14:57 +0100

    removed unsupported theme options

--- docs/conf.py
+++ docs/conf.py
@@ -159,8 +159,6 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "sidebar_width": "300px",
-    "page_width": "1200px"
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
