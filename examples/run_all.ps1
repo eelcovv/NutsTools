b6561fea1f7c0ddd7bae23aae7c02fe901024aa7
EVLT <evlt@cbs.nl>
2024-03-14 16:09:40 +0100

    unit tests now working

--- examples/run_all.ps1
+++ examples/run_all.ps1
@@ -7,3 +7,5 @@ postalcode2nuts -i postal_codes_NL.txt --verbose --level 0
 # first run for a country: update the settings
 postalcode2nuts -i postal_codes_BE.txt --verbose --country BE --update
 postalcode2nuts -i postal_codes_BE.txt --verbose --level 2
+postalcode2nuts -i postal_codes_BE.txt --verbose --level 1
+postalcode2nuts -i postal_codes_BE.txt --verbose --level 0
